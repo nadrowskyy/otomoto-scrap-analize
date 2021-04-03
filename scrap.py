9298e8ed3bccf9e3a81e129a4f39e424ccad8880
Tomasz <nadrowskia2@gmail.com>
2021-04-03 14:14:18 +0200

    add attributes file, date field to scrap file

--- scrap.py
+++ scrap.py
@@ -1,11 +1,24 @@
 import requests, bs4, pickle, pandas
+from datetime import datetime
+import locale
+locale.setlocale(locale.LC_ALL, 'pl_PL.UTF-8')
+
+lookup_table = {
+    "stycznia": "styczeń",   "lutego": "luty",
+    "marca": "marzec",       "kwietnia": "kwiecień",
+    "maja": "maj",           "czerwca": "czerwiec",
+    "lipca": "lipiec",       "sierpnia": "sierpień",
+    "września": "wrzesień",  "października": "październik",
+    "listopada": "listopad", "grudnia": "grudzień"
+}
+
 
 # GLOBAL DATA FRAME FOR ALL AUCTIONS
 DATA_FRAME = pandas.DataFrame(columns=['ID', 'Marka', 'Model', 'Moc', 'Poj. sil', 'Cena', 'Kraj poch.',
-                                        'Miasto', 'Wojewwdztwo','Czy zabytek', 'Czy bezwypadkowy', 'Serwisowany w ASO',
-                                        'Filtr DPF', 'Generacja', 'Rok prod.', 'Przebieg', 'Oferta od',
-                                        'Rodzaj paliwa', 'Emisja CO2', 'Typ', 'Kolor', 'Stan',
-                                        'Czy pierwsz. właśc', 'Napęd', 'Skrzynia biegów', 'Data dodania'])
+                'Miasto', 'Wojewwdztwo', 'Czy zabytek', 'Czy bezwypadkowy', 'Serwisowany w ASO',
+                'Filtr DPF', 'Generacja', 'Rok prod.', 'Przebieg', 'Oferta od', 'Leasing',
+                'Rodzaj paliwa', 'Emisja CO2', 'Typ', 'Kolor', 'Stan',
+                'Czy pierwsz. właśc', 'Napęd', 'Skrzynia biegów', 'Data dodania'])
 
 
 # SCRAPPING DATA FROM FOR EACH AUCTION
@@ -16,26 +29,30 @@ def scrap_data_for_offer(b, m, url, loc):
     brand = b
     model = m
     city = loc[0].strip()
+    # print(city)
     region = loc[1].strip('()')
+    # print(region)
 
     id_offer = None
     if soup.find_all('span', {'id':'ad_id'}):
         id_offer = soup.find_all('span', {'id':'ad_id'})[0].text
-    print('ID', id_offer)
+    # print('ID', id_offer)
 
     power = None
     if soup.find_all('span', string='Moc'):
-        power = soup.find_all('span', string='Moc')[0].parent.contents[3].text.strip()
+        power = int(soup.find_all('span', string='Moc')[0].parent.contents[3].text.strip().split(' ')[0])
     # print('Moc', power)
 
     eng_cap = None
     if soup.find_all('span', string='Pojemność skokowa'):
-        eng_cap = soup.find_all('span', string='Pojemność skokowa')[0].parent.contents[3].text.strip()
+        eng_cap = soup.find_all('span', string='Pojemność skokowa')[0].parent.contents[3].text.strip().split(' ')[:-1]
+        eng_cap = int(''.join(eng_cap))
     # print('Poj. sil.', eng_cap)
 
     price = None
     if soup.find_all('span', {'class':'offer-price__number'}):
-        price = soup.find_all('span', {'class':'offer-price__number'})[1].text.strip()
+        price = soup.find_all('span', {'class':'offer-price__number'})[1].text.strip().split(' ')[:-1]
+        price = int(''.join(price))
     # print('Cena', price)
 
     from_country = None
@@ -43,27 +60,32 @@ def scrap_data_for_offer(b, m, url, loc):
         from_country = soup.find_all('span', string='Kraj pochodzenia')[0].parent.contents[3].find('a').text.strip()
     # print('Kraj pochodzenia', from_country)
 
-    if_vintage = None
+    if_vintage = False
     if soup.find_all('span', string='Zarejestrowany jako zabytek'):
-        if_vintage = soup.find_all('span', string='Zarejestrowany jako zabytek')[0].parent.contents[3].find('a').text.strip()
+        if_vintage = soup.find_all('span', string='Zarejestrowany jako zabytek')[0].parent.contents[3].find('a')\
+            .text.strip()
+        if_vintage = True if if_vintage == 'Tak' else False
     # print('Czy zabytek', if_vintage)
 
     if_acc_free = None
     if soup.find_all('span', string='Bezwypadkowy'):
         if_acc_free = soup.find_all('span', string='Bezwypadkowy')[0].parent.contents[3].find(
             'a').text.strip()
+        if_acc_free = True if if_acc_free == 'Tak' else False
     # print('Bezwypadkowy', if_acc_free)
 
     if_aso = None
     if soup.find_all('span', string='Serwisowany w ASO'):
         if_aso = soup.find_all('span', string='Serwisowany w ASO')[0].parent.contents[3].find(
             'a').text.strip()
+        if_aso = True if if_aso == 'Tak' else False
     # print('Serwisowany w ASO', if_aso)
 
     if_dpf = None
     if soup.find_all('span', string='Filtr cząstek stałych'):
         if_dpf = soup.find_all('span', string='Filtr cząstek stałych')[0].parent.contents[3].find(
             'a').text.strip()
+        if_dpf = True if if_dpf == 'Tak' else False
     # print('Filtr cząstek stałych', if_dpf)
 
     generation = None
@@ -74,12 +96,13 @@ def scrap_data_for_offer(b, m, url, loc):
 
     prod_year = None
     if soup.find_all('span', string='Rok produkcji'):
-        prod_year = soup.find_all('span', string='Rok produkcji')[0].parent.contents[3].text.strip()
+        prod_year = int(soup.find_all('span', string='Rok produkcji')[0].parent.contents[3].text.strip())
     # print('Rok produkcji', prod_year)
 
     mileage = None
     if soup.find_all('span', string='Przebieg'):
-        mileage = soup.find_all('span', string='Przebieg')[0].parent.contents[3].text.strip()
+        mileage = soup.find_all('span', string='Przebieg')[0].parent.contents[3].text.strip().split(' ')[:-1]
+        mileage = int(''.join(mileage))
     # print('Przebieg', mileage)
 
     offer_from = None
@@ -87,6 +110,12 @@ def scrap_data_for_offer(b, m, url, loc):
         offer_from = soup.find_all('span', string='Oferta od')[0].parent.contents[3].find(
             'a').text.strip()
     # print('Oferta od', offer_from)
+
+    if_leasing = None
+    if soup.find_all('span', string='Leasing'):
+        if_leasing = soup.find_all('span', string='Leasing')[0].parent.contents[3].find('a').text.strip()
+        if_leasing = True if if_leasing == 'Tak' else False
+    # print('leasing', if_leasing)
 
     fuel_type = None
     if soup.find_all('span', string='Rodzaj paliwa'):
@@ -96,8 +125,9 @@ def scrap_data_for_offer(b, m, url, loc):
 
     co2_emission = None
     if soup.find_all('span', string='Emisja CO2'):
-        co2_emission = soup.find_all('span', string='Emisja CO2')[0].parent.contents[3].text.strip()
-    # print('Emisja CO2', co2_emission)
+        co2_emission = soup.find_all('span', string='Emisja CO2')[0].parent.contents[3].text.strip().split(' ')[:-1]
+        co2_emission = int(''.join(co2_emission))
+    # print('Emisja CO2 [g/kg]', co2_emission)
 
     car_type = None
     if soup.find_all('span', string='Typ'):
@@ -121,6 +151,7 @@ def scrap_data_for_offer(b, m, url, loc):
     if soup.find_all('span', string='Pierwszy właściciel'):
         if_first_owner = soup.find_all('span', string='Pierwszy właściciel')[0].parent.contents[3].find(
             'a').text.strip()
+        if_first_owner = True if if_first_owner == 'Tak' else False
     # print('Pierwszy właściciel', if_first_owner)
 
     drive = None
@@ -136,16 +167,21 @@ def scrap_data_for_offer(b, m, url, loc):
     # print('Skrzynia biegów', transmission_type)
 
     date = None
-    if soup.find_all('span', {'class': 'offer-meta__value'})[0]:
+    if soup.find_all('span', {'class': 'offer-meta__value'}):
         date = soup.find_all('span', {'class': 'offer-meta__value'})[0].string
+        for k, v in lookup_table.items():
+            date = date.replace(k, v)
+        date = datetime.strptime(date, '%H:%M, %d %B %Y')
+
+    print(date)
     # print('Data dodania', date)
     tmp_data_frame = pandas.DataFrame(
         [(id_offer, brand, model, power, eng_cap, price, from_country, city, region, if_vintage, if_acc_free, if_aso,
-          if_dpf, generation, prod_year, mileage, offer_from, fuel_type, co2_emission, car_type, color,
+          if_dpf, generation, prod_year, mileage, offer_from, if_leasing, fuel_type, co2_emission, car_type, color,
           condition, if_first_owner, drive, transmission_type, date)],
         columns=['ID', 'Marka', 'Model', 'Moc', 'Poj. sil', 'Cena', 'Kraj poch.',
                 'Miasto', 'Wojewwdztwo', 'Czy zabytek', 'Czy bezwypadkowy', 'Serwisowany w ASO',
-                'Filtr DPF', 'Generacja', 'Rok prod.', 'Przebieg', 'Oferta od',
+                'Filtr DPF', 'Generacja', 'Rok prod.', 'Przebieg', 'Oferta od', 'Leasing',
                 'Rodzaj paliwa', 'Emisja CO2', 'Typ', 'Kolor', 'Stan',
                 'Czy pierwsz. właśc', 'Napęd', 'Skrzynia biegów', 'Data dodania']
         )
@@ -183,9 +219,9 @@ def get_link_from_page(car_url, cars_dict):
                 cars_links = cars_soup.find_all('a', class_='offer-title__link')
                 cars_cities = cars_soup.find_all('span', class_='ds-location-city')
                 cars_regions = cars_soup.find_all('span', class_='ds-location-region')
-                print(len(cars_links))
-                print(len(cars_cities))
-                print(len(cars_regions))
+                # print(len(cars_links))
+                # print(len(cars_cities))
+                # print(len(cars_regions))
 
                 cars_link_dict = {}
                 for link, city, region in zip(cars_links, cars_cities, cars_regions):
